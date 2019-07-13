4ceecb692cdef2d6ec8557ca1c1106bea2fda090
ethanejohnsons <ethan@ejohnsons.com>
2019-07-13 12:21:50 -0500

    Updated drone spawner item

--- src/main/java/com/bluevista/fpvracing/items/ItemDrone.java
+++ src/main/java/com/bluevista/fpvracing/items/ItemDrone.java
@@ -1,15 +1,35 @@
 package com.bluevista.fpvracing.items;
 
-//import net.minecraft.creativetab.CreativeTabs;
+import com.bluevista.fpvracing.entities.DroneEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class ItemDrone extends Item {
 
 	public ItemDrone() {
 		super(new Item.Properties());
-//		this.setUnlocalizedName("drone_spawner");
-		this.setRegistryName("drone_spawner");
-//		this.setCreativeTab(CreativeTabs.COMBAT); TODO figure out creative tabs
+		this.setRegistryName("drone");
+	}
+
+	/**
+	 * Called when this item is used when targeting a Block
+	 */
+	public ActionResultType onItemUse(ItemUseContext context) {
+		PlayerEntity playerentity = context.getPlayer();
+		World world = context.getWorld();
+		if (!world.isRemote && playerentity != null) {
+			BlockPos blockpos = context.getPos();
+			DroneEntity drone = new DroneEntity(world);
+			drone.setLocationAndAngles(blockpos.getX(), blockpos.getY()+1, blockpos.getZ(), 0, 0);
+			world.addEntity(drone);
+//			this.handleClick(playerentity, world.getBlockState(blockpos), world, blockpos, true, context.getItem());
+		}
+
+		return ActionResultType.SUCCESS;
 	}
 	
 }
