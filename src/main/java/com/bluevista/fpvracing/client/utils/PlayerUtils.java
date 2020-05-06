aad35bae49c44f8bc3820c20c6f82c5f8db6de3b
unknown <ethan@ejohnsons.com>
2020-05-06 18:20:04 -0500

    Removed playerutils

--- src/main/java/com/bluevista/fpvracing/client/utils/PlayerUtils.java
+++
@@ -1,9 +0,0 @@
-package com.bluevista.fpvracing.client.utils;
-
-public class PlayerUtils {
-	
-//	public static boolean isPlayerFlyingDrone(EntityPlayer drone) {
-//		return Minecraft.getMinecraft().player.isRiding() && Minecraft.getMinecraft().player.getRidingEntity() instanceof EntityDrone;
-//	}
-	
-}
